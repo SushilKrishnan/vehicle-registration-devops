d9f6f8d51f3a6e31c1685b28068cd96e8fc732db
mem1 <mem1@team.com>
2026-03-09 19:32:28 +0530

    Initial Maven project created

---
+++ src/main/java/com/vehicle/vehicleregistrationsystem/VehicleRegistrationSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vehicle.vehicleregistrationsystem;
+
+/**
+ *
+ * @author Sushil Krishnan
+ */
+public class VehicleRegistrationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
